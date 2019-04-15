45c1f15b6c3d3241ee48faf17e5aeb58645a3884
Jesus Dario Marenco Porto <jesusdario10@hotmail.com>
2019-04-15 14:21:32 -0500

    se crean componentes sin nada se agregan los archivos a la carpeta assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
